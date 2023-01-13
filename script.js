e94a4602667455c19946b2961daa6f053db07d1f
shivani sawarkar <117519427+shivanisawarakar@users.noreply.github.com>
2023-01-13 21:51:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your code here
+let textarea = document.querySelector('#evaluatedText');
+let tcount = document.querySelector('#tcount');
+let wcount = document.querySelector('#wcount');
+let scount = document.querySelector('#scount');
+
+function keycount() {
+
+
+
+    let word = textarea.value.split(/\s+/).filter(e =>{return (e.length !== 0)} ).length
+
+     wcount.innerHTML = word;
+
+    let textcount = textarea.value.replace(/\s|\n/g, "").length;
+
+     tcount.innerHTML = textcount;
+
+  
+}
